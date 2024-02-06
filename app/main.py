9ef7f425e86cfc4f76a06c7cb0b18cc51f8cdab0
Alexandre Disdier <yeye.disdier@gmail.com>
2024-02-06 11:27:22 +0100

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -1,15 +1,27 @@
 # Uncomment this to pass the first stage
+from http import HTTPStatus
 import socket
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    ADDRESS = "localhost"
+    PORT = 4221
+    HTTP_VERSION = "HTTP/1.1"
+    # https://developer.mozilla.org/en-US/docs/Glossary/CRLF
+    CRLF = "\r\n"
+    server_socket = socket.create_server(
+        (ADDRESS, PORT), 
+        family=socket.AF_INET, # AF_INET is for IPv4 https://man7.org/linux/man-pages/man2/socket.2.html
+        reuse_port=True
+    )
+    while True:
+        client_connection, address = server_socket.accept()
+        print(f"Connection from {address} has been accepted.")
+        request_data = client_connection.recv(1024)
+        print(request_data.decode("utf-8"))
+        status_line = f"{HTTP_VERSION} {HTTPStatus.OK} OK{CRLF}{CRLF}"
+        client_connection.sendall(status_line.encode())
+        client_connection.close()
 
 
 if __name__ == "__main__":
