327bed1a898d9857bad590297a6714ec15e777dc
Alexandre Disdier <yeye.disdier@gmail.com>
2024-02-24 21:01:29 +0100

    Test 1

--- app/main.py
+++ app/main.py
@@ -164,6 +164,7 @@ def main():
     args = parse_args()
     global directory
     directory = args.directory
+    print('Directory', directory)
     routes = {
         "/" : stage_3,
         "/echo" : stage_4,
